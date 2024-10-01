c4612c25457034b024ec40b1a3909c6cde81af9d
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-01 16:41:14 +0200

    Removed redundant AuthenticatedRequest definitions and imported the existing one. Added verifyAdmin middleware for admin verification in certain routes.

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { admin } from '.././config/firebase'; // Import the initialized Firebase Admin instance
+import { AuthenticatedRequest } from '../types/authenticatedRequest';
+import { User } from '../models/User';
+import { UserRole } from '../types/user';
 
-// Extend the Request interface to include a 'user' property
-interface AuthenticatedRequest extends Request {
-  user?: { uid: string };
-}
 
-export const verifyToken = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const authorizationHeader = req.headers.authorization; // Get the token from the Authorization header
 
   if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
@@ -18,10 +18,22 @@ export const verifyToken = async (req: AuthenticatedRequest, res: Response, next
     // Verify the token using Firebase Admin
     const decodedToken = await admin.auth().verifyIdToken(token);
     console.log('decodedToken', decodedToken);
-    (req as any).user = decodedToken; // Attach the decoded token to the request object
+    (req as AuthenticatedRequest).user = decodedToken; // Attach the decoded token to the request object
     next();
   } catch (error) {
     console.error(error);
     return res.status(401).json({ message: 'Unauthorized: Invalid token' });
   }
+};
+
+export const verifyAdmin = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    return res.status(403).json({ message: 'Forbidden: login required' });
+  }
+  const firebaseUserId = req.user.uid;
+  const user = await User.findOne({ firebaseUserId });
+  if (user?.role !== UserRole.ADMIN) {
+    return res.status(403).json({ message: 'Forbidden: admin role required' });
+  }
+  next();
 };
