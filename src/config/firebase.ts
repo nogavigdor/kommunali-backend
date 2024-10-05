c145012c5f03f11d7a7c6c4f52514642248174ba
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-06 01:44:19 +0200

    convert Firebase JSON credentials to environment variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,9 +2,23 @@ import admin from 'firebase-admin';
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import * as firebase from 'firebase/app';
 import dotenv from 'dotenv';
-import serviceAccountKey from './firebase-adminsdk.json'; // Directly import the JSON file
+//import serviceAccountKey from './firebase-adminsdk.json'; // Directly import the JSON file
 
 dotenv.config(); // Load environment variables
+
+// Initialize Firebase Admin SDK with environment variables
+const serviceAccountKey = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newlines in the private key
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
 
 // Initialize Firebase Admin SDK with the service account credentials
 admin.initializeApp({
