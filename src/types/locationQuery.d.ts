4fd4cbc70f50413818e26993efbbd9b10d017d93
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-05 21:29:41 +0200

    converted the polygon search into a box search and therefor it relays only on two points instead of four

--- src/types/locationQuery.d.ts
+++ src/types/locationQuery.d.ts
@@ -1,8 +1,16 @@
 import { Types } from 'mongoose';
-
+/* can be used to define a plygon with 4 points */
+/*
 export interface ILocationQuery {
   topLeft: [number, number];
   topRight: [number, number];
   bottomRight: [number, number];
   bottomLeft: [number, number];
+}
+*/
+
+/* define a rectangle/screen with 2 points */
+export interface ILocationQuery {
+  bottomLeft: [number, number];
+  topRight: [number, number];
 }
