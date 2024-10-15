9de18d382a7904034c24332782f7e6b4a2a6dd76
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-15 18:53:34 +0200

    Added AuthenticatedMongoRequest in order to add support for MongoDB user retrieval in authenticated requests

--- src/types/authenticatedRequest.d.ts
+++ src/types/authenticatedRequest.d.ts
@@ -1,7 +1,12 @@
 import type { Request } from 'express';
 import type { DecodedIdToken } from 'firebase-admin/auth';
+import { IUser } from './user';
 
 // Extend the Request interface to include a 'user' property which contains firebase user data
 export interface AuthenticatedRequest extends Request {
     user?: DecodedIdToken;
+  }
+
+export interface AuthenticatedMongoRequest extends AuthenticatedRequest {
+    mongoUser?: IUser;
   }
