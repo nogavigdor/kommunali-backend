c3dd4bdc4ab2cac847c30eb39528643349fd261f
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-26 02:54:15 +0100

    Added user login validation schema

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -16,3 +16,18 @@ export const userRegistrationSchema = Joi.object({
 export const validateRegistration = (data: IRegisterUser) => {
   return userRegistrationSchema.validate(data, { abortEarly: false });
 };
+
+export const userLoginSchema = Joi.object({
+    email: Joi.string().email().required().messages({
+        'string.email': 'Invalid email format',
+        'string.empty': 'Email is required',
+    }),
+    password: Joi.string().min(8).required().messages({
+        'string.min': 'Password must be at least 8 characters',
+        'string.empty': 'Password is required',
+    }),
+    });
+
+export const validateLogin = (data: IRegisterUser) => {
+    return userLoginSchema.validate(data, { abortEarly: false });
+}
