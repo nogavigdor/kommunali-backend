473190a866e56ad3b2722316e84c8ecdeae2fdb8
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-07 17:38:14 +0100

    uncommented Algolia client initialization and rename the ALGOLIA_ADMIN_API_KEY to ALGOLIA_WRITE_API_KEY  for better readability

--- src/config/algolia.ts
+++ src/config/algolia.ts
@@ -1,9 +1,12 @@
-/* import * as algoliasearch from 'algoliasearch'; // Correct import
+import algoliasearch from 'algoliasearch';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // Initialize the Algolia client
-const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID as string, process.env.ALGOLIA_ADMIN_API_KEY as string);
+//this typing error should be ignore as algolia search provides its own types
+const algoliaClient = algoliasearch(
+    process.env.ALGOLIA_APP_ID as string,
+     process.env.ALGOLIA_WRITE_API_KEY as string
+    );
 export const productsIndex = algoliaClient.initIndex('products');
-*/
