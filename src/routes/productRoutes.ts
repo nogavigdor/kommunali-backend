88b477104eb96aaa03e98102f8b2788de796ef7b
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-15 10:43:29 +0100

    Added route to approve product requests by the shops owners.

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   addProduct,
   addProductRequest,
+  approveProductRequest,
   getAllProducts,
   getProductById,
   updateProductDetails,
@@ -10,6 +11,7 @@ import {
 } from '../controllers/productController';
 import { verifyToken, addMongoUserToRequest } from '../middlewares/authMiddleware';
 import { verifyStoreOwner } from '../middlewares/storeMiddleware';
+import { app } from 'firebase-admin';
 
 
 const router = express.Router({ mergeParams: true });
@@ -19,6 +21,9 @@ router.post('/', verifyToken, verifyStoreOwner, addProduct);
 
 // Add a request for a specific product
 router.put('/:productId/request', verifyToken, addMongoUserToRequest, addProductRequest);
+
+// Approve a request for a specific product (only the store owner can approve requests)
+router.put('/:productId/approve', verifyToken, verifyStoreOwner, approveProductRequest);
 
 // Get all products (with optional filtering)
 router.get('/', getAllProducts);
