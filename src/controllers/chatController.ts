dcf93d583fd736b7551273bac5901b1f345eba5d
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-22 23:50:08 +0100

    added comments to createChat controller.

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -12,7 +12,6 @@ if (!firebase.apps.length) {
     firebase.initializeApp(firebaseClientConfig);
   }
 
-
 // create a chat witha a store
 export const createChat = async (req: AuthenticatedRequest, res: Response) => {
     try {
@@ -20,20 +19,20 @@ export const createChat = async (req: AuthenticatedRequest, res: Response) => {
         if (!shopId || !productId) {
             return res.status(400).json({ message: 'Missing required fields' });
         }
-
         const firebaseUserId = req.user?.uid;
-
         if (!firebaseUserId) {
             return res.status(401).json({ message: 'Unauthorized: No user ID found' });
         }
-
+        // Create a new chat document in Firestore
         const fireStore = admin.firestore();
         const shopDoc = fireStore.collection('shopChats').doc(shopId);
         const chatsCollection = shopDoc.collection('chats');
+        // Add a new chat document to the chats collection
         const chatDoc = await chatsCollection.add({
             customer: firebaseUserId,
             product: productId,
         });
+        // Update the user profile with the new chat
         const user = await User.findOneAndUpdate({ firebaseUserId }, {
             $push: {
                 chatsInitiated: {
@@ -42,7 +41,6 @@ export const createChat = async (req: AuthenticatedRequest, res: Response) => {
                 },
             },
         });
-
         res.status(200).json({ user });
     } catch (error) {
         res.status(500).json({ message: 'Failed to create chat', error });
